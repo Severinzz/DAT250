64edbee9e8941ce2670cc6f6432c1434308f94b3
Severinzz <36744838+Severinzz@users.noreply.github.com>
2020-09-29 16:07:18 +0200

    Exp 5 complete

---
+++ exp5/exp4/src/main/java/no/hvl/dat250/expass5/exp4/CustomerRepository.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.expass5.exp4;
+
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
